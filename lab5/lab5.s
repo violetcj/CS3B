678ca5fcc0ad7a1e6f4e92f3b8f9ec484b9cf214
CJ Carroll <cjscarroll0229@gmail.com>
2021-09-17 01:16:50 -0400

    Lab 5 done

--- lab5/lab5.s
+++ lab5/lab5.s
@@ -1,24 +1,38 @@
 .data 
 
-szX: 	.asciz 		"51"
-szY:	.asciz		"9"
+szX: 	.asciz 		"10"
+szY:	.asciz		"15"
 szAdd:	.asciz		" + "
-chCr: 	.byte		10
-dbX:	.dword		0
+szSum:	.asciz		" = ?? "
+dbX:	.word		0
+dbY:	.word		0
+dbSum:	.word		0
 
 .global _start		//Provide program starting address to linker
 .text
 
-_start
+_start:
 	//convert szX to an integer and add it to the accumulator register
 	LDR	X0, =szX	//X0 =&szX
 	BL	ascint64	//BL branch Link stores the next LOC in the link register 
 				//Upon return X0 = ascint64(*X0)
-	LDR X0, =szY		//X0 = szY
-	BL ascint64 		//bl Branch link stores the next LOC in the link register
+
+	LDR	 X10, =dbX	//X10 = &dbX
+	STR	 X0, [X10]	//stores the dereferenced value of X10 into X0
+	MOV	 X13, X0	//value of X0 moved into X13
+
+	LDR 	X0, =szY	//X0 = &szY
+	BL 	ascint64 	//bl Branch link stores the next LOC in the link register
 				//Upon return X0 = ascint64(*X0)
 				//Code process to X + Y ... registers X10 and above can be safely used
 
+	LDR	X12, =dbY	//X12 = &dbY
+	STR 	X0, [X12]	//stores dereferenced value of X12 into X0
+	MOV 	X14, X0		//value of X0 moved into X14
+
+	ADD	X15, X13, X14	//Adds X13 and X14 and stores into X15
+	LDR 	X16, =dbSum	//X16 = &dbSum
+	STR	X15, [X16]	//dereferenced value of X16 into X15
 
 
 //PRINT RESULTS
@@ -29,17 +43,47 @@ _start
 	MOV	X8, #64		//linux write system call
 	svc 0			//call linux to output string 
 
+	MOV	X0, #1		//1 = StdOut
+	LDR 	X1, =szAdd	//string to print
+	MOV	X2, #3		//length of string
+	MOV	X8, #64		//linux write system call
+	svc 0			//call linux to output string
+
+	MOV 	X0, #1		//1 = StdOut
+	LDR	X1, =szY	//string to print
+	MOV	X2, #2		//length of string 
+	MOV	X8, #64		//linux write system call
+	svc 0			//call linux to output string
+
+	MOV	X0, #1		//1 = StdOut
+	LDR 	X1, =szSum	//string to print 
+	MOV	X2, #6		//length of string
+	MOV	X8, #64		//linux write system call
+	svc 0			//call linux to output string 
+
 exit:
 	//setup parameters to exit the program and then call linux to do it
 	MOV X0, #0		//use 0 return code 
 	MOV X8, #93		//service command code 93 terminates the program
 	svc 0			//call linux to terminate program 
 
+//ascint64: converts string of characters to an equivalent 8-byte value. The binary
+//	    value of the converted string us returned in the X0 register. If there is 
+//	    an invalid character in the numeric string which would invalidate the conversion
+//	    the carry flag is set to 1 and the X0 rtegister returns 0.
+//	    If the numeric string contains a number that is too large to fit in a dword, the 
+//	    overflow is set to 1 and the X0 register returns 0
+//X0: must point to a null terminated string
+//LR: contains return address 
+
+//Returned register contents :
+//	X0: Decimal result
+//Registers X0 - X8 are modified and not preserved
 
 ascint64:
 	MOV	X8, X0		//Preserve X0 since strlength will overwrite it
 	MOV 	X9, LR		//Preserve LR before losinjg it to strlength
-	BL	strlength	//X0 = strlength(*X0)
+	BL	strLength	//X0 = strlength(*X0)
 	MOV 	LR, X9		//restore link register
 
 	MOV	X5, X0		// Copy X0 (length) into X5
@@ -56,7 +100,7 @@ topLoop:
 	MOV	X7, #0
 	ADD	X7, X7, X0	//point to the last digit in *X0 i.e 1's
 	ADD	X7, X7, X5	//X7 -> to the end of the cstring so we can work backwards
-	SUB	X7, X7 #1	//
+	SUB	X7, X7, #1	//
 	LDRB	W1, [X7], #1	//indirect addressing X1 = *X0
 
 	SUB	W1, W1, #0X30	//subtract the ascii offset of 48
